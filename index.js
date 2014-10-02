87b634ef6c3a64c9fd564fc82304c18ef2e28a79
Ragesh Krishna <ragesh@rageshkrishna.com>
2014-10-02 14:42:16 +0530

    Use streams instead of callbacks

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
-var Generator = require('./lib/generator.js');
+var generatePosts = require('./lib/generator.js'),
+    logger = require('./lib/logger.js');
 
-var g = new Generator();
-g.generateHtml();
+var postStream = generatePosts();
+
+postStream.on('error', function(err) {
+  logger.error(err, 'Error from post stream');
+});
+postStream.on('readable', function() {
+  logger.debug('Reading post stream');
+  var post = postStream.read();
+  logger.debug({ post: post }, 'Got a post');
+});
+postStream.on('end', function() {
+  logger.debug('Post stream processed completely');
+});
+
+
